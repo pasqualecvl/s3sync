62c759e742ba5bd3fdfaa367d5222d68119c02fc
Pasquale Cavallo <info@pasqualecavallo.it>
2021-10-16 02:58:06 +0200

    list object

--- src/main/java/it/pasqualecavallo/s3sync/service/TrashService.java
+++ src/main/java/it/pasqualecavallo/s3sync/service/TrashService.java
@@ -1,27 +1,51 @@
 package it.pasqualecavallo.s3sync.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import it.pasqualecavallo.s3sync.utils.GlobalPropertiesManager;
-import it.pasqualecavallo.s3sync.utils.UserSpecificPropertiesManager;
-import it.pasqualecavallo.s3sync.web.controller.NavigateTrashResponse;
+import it.pasqualecavallo.s3sync.web.dto.response.Folder;
 import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.ListObjectsRequest;
 import software.amazon.awssdk.services.s3.model.ListObjectsV2Request;
+import software.amazon.awssdk.services.s3.model.ListObjectsV2Response;
+import software.amazon.awssdk.services.s3.model.S3Object;
 
 @Service
 public class TrashService {
-	
+
 	@Autowired
 	private S3Client s3Client;
 
-	public SearchTrashResponse search(String relativePath) {
-		ListObjectsRequest request = ListObjectsV2Request
-				.builder()
-				.bucket(GlobalPropertiesManager.getProperty("s3.bucket"))
-				.
-		s3Client.listObjects(request)
+	public Folder navigate(String relativePath) {
+		String s3bucket = GlobalPropertiesManager.getProperty("s3.bucket");
+
+		ListObjectsV2Request request = ListObjectsV2Request.builder().bucket(s3bucket)
+				.prefix(relativePath).build();
+		List<String> keys = new ArrayList<>();
+		ListObjectsV2Response list = null;
+		do {
+			list = s3Client.listObjectsV2(request);
+			for (S3Object key : list.contents()) {
+				keys.add(key.key());
+			}
+			request = ListObjectsV2Request.builder().bucket(s3bucket).prefix(relativePath)
+					.continuationToken(list.nextContinuationToken()).build();
+		} while (list.isTruncated());
+		
+		return folderize(keys);
+	}
+
+	private Folder folderize(List<String> keys) {
+		Folder response = new Folder();
+		for(String key : keys) {
+			String[] tokenized = key.split("/");
+			Folder f = response.addNodes(tokenized);
+			f.addFile(tokenized[tokenized.length - 1]);
+		}
+		return response;
 	}
 
 }
--- src/main/java/it/pasqualecavallo/s3sync/web/controller/TrashController.java
+++ src/main/java/it/pasqualecavallo/s3sync/web/controller/TrashController.java
@@ -1,13 +1,12 @@
 package it.pasqualecavallo.s3sync.web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import it.pasqualecavallo.s3sync.service.TrashService;
+import it.pasqualecavallo.s3sync.web.dto.response.Folder;
 
 @RestController
 public class TrashController {
@@ -16,18 +15,18 @@ public class TrashController {
 	private TrashService trashService;
 	
 	@GetMapping(value="/api/trash/navigate")
-	public NavigateTrashResponse navigateTrash(@RequestParam String relativePath) {
+	public Folder navigateTrash(@RequestParam String relativePath) {
 		return trashService.navigate(relativePath);
 	}
 	
-	@DeleteMapping(value="/api/trash/delete")
-	public DeleteTrashItemResponse deleteTrashedFile() {
-		
-	}
-	
-	@PostMapping(value="/api/trash/recover")
-	public MoveTrashedFileResponse moveTrashedFile() {
-		
-	}
+//	@DeleteMapping(value="/api/trash/delete")
+//	public DeleteTrashItemResponse deleteTrashedFile() {
+//		
+//	}
+//	
+//	@PostMapping(value="/api/trash/recover")
+//	public MoveTrashedFileResponse moveTrashedFile() {
+//		
+//	}
 	
 }
