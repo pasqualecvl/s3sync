1c78450beb1fda99c26ad719bf313583e81edebd
Giovanni Cavallo <giovannicavallo95@gmail.com>
2021-10-23 15:16:31 +0200

    scaffold fe

---
+++ fe/src/components/HelloWorld.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="">Hello S3Sync</div>
+</template>
+
+<script setup>
+import { ref } from "vue"
+
+defineProps({})
+
+const count = ref(0)
+</script>
+
+<style scoped></style>
