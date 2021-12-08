84be9f2b6cdf962eb3b5d9948194ce6d41c43046
Pasquale Cavallo <info@pasqualecavallo.it>
2021-12-08 18:26:32 +0100

    Fix pagination

--- src/main/java/it/s3sync/utils/ListUtils.java
+++ src/main/java/it/s3sync/utils/ListUtils.java
@@ -6,11 +6,15 @@ import java.util.List;
 public class ListUtils {
 
 	public static <T> List<T> getPageOf(List<T> list, Integer page, Integer pageSize) {
-		List<T> sublist = null;
-		int listSize = list != null ? list.size() : 0;
-		if(listSize == 0) {
-			sublist = new ArrayList<>();
+		if(page == null || page < 1) { // -> handling page number
+			page = 1;
 		}
+		if(pageSize == null || pageSize < 1) {
+			pageSize = 20; //use default pagesize
+		}
+		page = page  - 1; // -> move to zero based
+		List<T> sublist = new ArrayList<>();
+		int listSize = list != null ? list.size() : 0;
 		if (listSize != 0 && page < (int) Math.ceil((double) listSize / (double) pageSize)) {
 			if (listSize > ((page + 1) * pageSize)) {
 				sublist = list.subList(pageSize * page, pageSize * (page + 1));
